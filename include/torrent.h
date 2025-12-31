d34ef966fba2dfd6a5e8e1c4e5a116657fbf0db9
Fabry <larosaf87@gmail.com>
2025-12-31 21:29:19 +0100

    redesigned the api

--- include/torrent.h
+++ include/torrent.h
@@ -1,7 +1,13 @@
 #pragma once
 
 #include "bencode.h"
+#include <chrono>
 #include <expected>
+#include <filesystem>
+#include <optional>
+#include <string>
+#include <variant>
+#include <vector>
 
 namespace btc::Torrent {
 
@@ -9,19 +15,24 @@ class Error {
 
 public:
   enum err_code {
+    errorOpeningFileErr,
     missingAnnounceKeyErr,
     missingInfoKeyErr,
     missingNameFieldErr,
     missingPieceLengthFieldErr,
     missingPiecesFieldErr,
 
-    announceKeyNotMappedToString,
+    rootStructureNotDictErr,
+    announceKeyNotMappedToStringErr,
     infoKeyNotMappedToDictErr,
     nameFieldNotMappedToStrErr,
     pieceLengthFieldNotMappedToIntErr,
     piecesFieldNotMapppedToStrErr,
     lengthFieldNotMappedToIntErr,
-    filesFieldNotMappedToList,
+    filesFieldNotMappedToListErr,
+    lengthFieldInFilesItemNotMappedToIntErr,
+    pathFieldInFilesItemNotMappedToListErr,
+    pathFragmentInFilesItemNotMappedToStrErr,
 
     pieceLengthNegativeErr,
     pieceLengthZeroErr,
@@ -31,13 +42,11 @@ public:
     filesFieldItemIsNotDictErr,
     missingLengthFieldInFilesItemErr,
     missingPathFieldInFilesItemErr,
-    lengthFieldInFilesItemNotMappedToIntErr,
-    pathFieldInFilesItemNotMappedToStrErr,
 
     bothLengthAndFilesFieldsMissingErr,
     bothLengthAndFilesFieldsPresentErr,
-
   };
+
   Error(err_code code);
   std::string get();
 
@@ -45,20 +54,28 @@ public:
 
 private:
   const std::unordered_map<err_code, std::string> err_mess = {
+      {rootStructureNotDictErr,
+       "the root structure is not mapped to a dictionary"},
       {missingAnnounceKeyErr, "Announce key is missing."},
       {missingInfoKeyErr, "Info key is missing."},
       {missingNameFieldErr, "Name field is missing in the info dictionary."},
       {missingPieceLengthFieldErr, "Piece length field is missing."},
       {missingPiecesFieldErr, "Pieces field is missing."},
 
-      {announceKeyNotMappedToString, "Announce key is not a string."},
+      {announceKeyNotMappedToStringErr, "Announce key is not a string."},
       {infoKeyNotMappedToDictErr, "Info key is not mapped to a dictionary."},
       {nameFieldNotMappedToStrErr, "Name field is not a string."},
       {pieceLengthFieldNotMappedToIntErr,
        "Piece length field is not an integer."},
       {piecesFieldNotMapppedToStrErr, "Pieces field is not a string."},
       {lengthFieldNotMappedToIntErr, "Length field is not an integer."},
-      {filesFieldNotMappedToList, "Files field is not a list."},
+      {filesFieldNotMappedToListErr, "Files field is not a list."},
+      {lengthFieldInFilesItemNotMappedToIntErr,
+       "Length field in files item is not an integer."},
+      {pathFieldInFilesItemNotMappedToListErr,
+       "Path field in files item is not a list."},
+      {pathFragmentInFilesItemNotMappedToStrErr,
+       "path fragment inside file dict in file list is not a string"},
 
       {pieceLengthNegativeErr, "Piece length is negative."},
       {pieceLengthZeroErr, "Piece length is zero."},
@@ -70,10 +87,6 @@ private:
        "An item in files field is not a dictionary."},
       {missingLengthFieldInFilesItemErr, "Length field missing in files item."},
       {missingPathFieldInFilesItemErr, "Path field missing in files item."},
-      {lengthFieldInFilesItemNotMappedToIntErr,
-       "Length field in files item is not an integer."},
-      {pathFieldInFilesItemNotMappedToStrErr,
-       "Path field in files item is not a string."},
 
       {bothLengthAndFilesFieldsMissingErr,
        "Both length and files fields are missing."},
@@ -82,27 +95,59 @@ private:
   };
 };
 
-class Torrent {
+typedef struct {
+  std::size_t length;
+  std::filesystem::path path;
+} FileInfo;
+
+typedef struct TorrentInfo {
+  std::string name{};
+  std::size_t pieceLength{};
+  std::string pieces{};
+  bool private_{};
+
+  std::optional<std::size_t> length{};
+  std::optional<std::vector<FileInfo>> files{};
+} TorrentInfo;
+
+class TorrentFile {
 
 private:
-  using expected_bool = std::expected<bool, Error>;
-  using bencode_dict = btc::Bencode::Value::Dict;
-  using bencode_list = btc::Bencode::Value::List;
-  using expected_val = std::expected<Torrent, Error>;
-  bencode_dict root;
+  using date = std::chrono::year_month_day;
 
-  Torrent(bencode_dict root) : root(root) {}
-
-  static expected_bool isAnnounceValid(bencode_dict *root);
-  static expected_bool isInfoValid(bencode_dict *root);
-  static expected_bool isNameValid(bencode_dict *info);
-  static expected_bool isPieceLengthValid(bencode_dict *info);
-  static expected_bool isPiecesValid(bencode_dict *info);
-  static expected_bool isLengthOrFilesValid(bencode_dict *info);
-  static expected_bool isFilesListValid(bencode_list *files);
+  std::string announce{};
+  std::optional<std::vector<std::string>> announceList{};
+  TorrentInfo info{};
+  std::optional<date> creationDate{};
+  std::optional<std::string> comment{};
+  std::optional<std::string> createdBy{};
+  std::optional<std::string> encoding{};
+  std::string infoHash{};
 
 public:
-  static expected_val validate(bencode_dict *root);
+  TorrentFile() {}
+
+  void setAnnounce(std::string);
+  void setAnnounceList(std::vector<std::string>);
+  void setInfo(TorrentInfo);
+  void setCreationDate(date);
+  void setComment(std::string);
+  void setCreatedBy(std::string);
+  void setEncoding(std::string);
+};
+
+class TorrentParser {
+private:
+  using torrentError = std::variant<Error, Bencode::Error>;
+
+public:
+  static std::expected<TorrentFile, torrentError>
+  parseFile(std::filesystem::path path, Bencode::Decoder decoder);
+
+private:
+  static std::vector<FileInfo> parseFiles(Bencode::Value files);
+  static std::vector<std::string>
+  parseAnnounceList(Bencode::Value announceList);
 };
 
 } // namespace btc::Torrent
