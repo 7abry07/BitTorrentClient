0db323dab8659fa06c8edf2cc29ed868f7be830c
Fabry <larosaf87@gmail.com>
2026-01-28 15:23:53 +0100

    added get method to http connection

--- include/Net/httpConnection.h
+++ include/Net/httpConnection.h
@@ -3,6 +3,11 @@
 #include <boost/asio/awaitable.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/core/tcp_stream.hpp>
+#include <boost/beast/http/dynamic_body_fwd.hpp>
+#include <boost/beast/http/message_fwd.hpp>
+#include <boost/beast/http/string_body_fwd.hpp>
 #include <boost/system/detail/error_code.hpp>
 #include <cstdint>
 #include <errors.h>
@@ -10,24 +15,30 @@
 
 namespace btc {
 
-using boost::asio::awaitable;
-using boost::asio::io_context;
-using boost::asio::ip::tcp;
-using boost::system::error_code;
+namespace http = boost::beast::http;
+namespace beast = boost::beast;
+namespace net = boost::asio;
+namespace sys = boost::system;
+
+using net::awaitable;
+using net::io_context;
+using net::ip::tcp;
 
 class HttpConnection {
 
 public:
-  static awaitable<std::expected<HttpConnection, error_code>>
+  static awaitable<std::expected<HttpConnection, sys::error_code>>
   connect(io_context &ctx, std::string hostname, std::uint16_t port);
+
+  awaitable<http::response<http::dynamic_body>> get(std::string url);
 
 private:
   HttpConnection(io_context &ctx, std::string hostname, std::uint16_t port)
-      : ctx(ctx), resolver(ctx), socket(ctx), hostname(hostname), port(port) {}
+      : ctx(ctx), resolver(ctx), stream(ctx), hostname(hostname), port(port) {}
 
   io_context &ctx;
   tcp::resolver resolver;
-  tcp::socket socket;
+  beast::tcp_stream stream;
 
   std::uint16_t port;
   std::string hostname;
--- src/Net/httpConnection.cpp
+++ src/Net/httpConnection.cpp
@@ -1,16 +1,26 @@
 #include <Net/httpConnection.h>
 #include <boost/asio.hpp>
+#include <boost/asio/redirect_error.hpp>
+#include <boost/asio/use_awaitable.hpp>
+#include <boost/beast/core/flat_buffer.hpp>
+#include <boost/beast/http/dynamic_body_fwd.hpp>
+#include <boost/beast/http/impl/read.hpp>
+#include <boost/beast/http/impl/write.hpp>
+#include <boost/beast/http/message_fwd.hpp>
+#include <boost/beast/http/string_body_fwd.hpp>
+#include <boost/beast/http/verb.hpp>
+#include <boost/system/detail/error_code.hpp>
 #include <expected>
 #include <string>
 
 namespace btc {
 
-awaitable<std::expected<HttpConnection, error_code>>
+awaitable<std::expected<HttpConnection, sys::error_code>>
 HttpConnection::connect(io_context &ctx, std::string hostname,
                         std::uint16_t port) {
   HttpConnection conn(ctx, hostname, port);
 
-  error_code ec;
+  sys::error_code ec;
   auto endpoints = co_await conn.resolver.async_resolve(
       hostname, std::to_string(port),
       boost::asio::redirect_error(boost::asio::use_awaitable, ec));
@@ -18,14 +28,48 @@ HttpConnection::connect(io_context &ctx, std::string hostname,
   if (ec)
     co_return std::unexpected(ec);
 
-  co_await boost::asio::async_connect(
-      conn.socket, endpoints,
-      boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+  co_await conn.stream.async_connect(
+      endpoints, boost::asio::redirect_error(boost::asio::use_awaitable, ec));
 
   if (ec)
     co_return std::unexpected(ec);
 
   co_return conn;
+}
+
+awaitable<http::response<http::dynamic_body>>
+HttpConnection::get(std::string url) {
+  auto hostnamePos = url.find("://");
+  if (hostnamePos == std::string::npos)
+    co_return http::response<http::dynamic_body>();
+
+  auto rest = url.substr(hostnamePos + 3);
+  auto targetPos = rest.find("/");
+  if (hostnamePos == std::string::npos)
+    co_return http::response<http::dynamic_body>();
+
+  std::string target = rest.substr(targetPos);
+
+  http::request<http::string_body> req{http::verb::get, target, 11};
+  req.set(http::field::host, hostname);
+  req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+  sys::error_code ec;
+  co_await http::async_write(stream, req,
+                             net::redirect_error(net::use_awaitable, ec));
+  if (ec)
+    co_return http::response<http::dynamic_body>();
+
+  beast::flat_buffer buf;
+  http::response<http::dynamic_body> resp;
+
+  co_await http::async_read(stream, buf, resp,
+                            net::redirect_error(net::use_awaitable, ec));
+
+  if (ec)
+    co_return http::response<http::dynamic_body>();
+
+  co_return resp;
 }
 
 } // namespace btc
